7ce7e9115d2e64ca6c2d0d1394331e515d4c0963
Dylan O'Shea <dylan.j.oshea@gmail.com>
2013-07-22 09:12:01 -0400

    Initializing repo without TestUnit and with Postgres

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Catfax::Application.config.secret_key_base = '4749af9e699d0bea44f028214f623c21e55a74a3820c4fe4b1e1a1d55862b1f19b53221d8129f1f0f7e4746f3e94175c5584d77b72ddc72405b7e46d9eea1adb'
